eec8091bd3cfcc6440e42f809a06edcb663c3393
Caroline Joy Pickett <cj@prodical.us>
2025-07-06 22:39:41 -0500

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -5,6 +5,7 @@
 project = 'OHIS Radio KISS'
 copyright = '2025, un-Creatrix'
 author = 'un-Creatrix'
+github_url = 'https://github.com/un-creatrix/OHIS-Radio_KISS'
 
 release = '0.0'
 version = '0.0.0'
@@ -24,6 +25,24 @@ intersphinx_mapping = {
     'python': ('https://docs.python.org/3/', None),
     'sphinx': ('https://www.sphinx-doc.org/en/master/', None),
 }
+
+html_theme_options = {
+    'logo_only': False,
+    'prev_next_buttons_location': 'bottom',
+    'style_external_links': False,
+    'vcs_pageview_mode': 'blob',
+    'style_nav_header_background': 'white',
+    'flyout_display': 'hidden',
+    'version_selector': True,
+    'language_selector': False,
+    # Toc options
+    'collapse_navigation': False,
+    'sticky_navigation': True,
+    'navigation_depth': 3,
+    'includehidden': True,
+    'titles_only': False
+}
+
 intersphinx_disabled_domains = ['std']
 
 templates_path = ['_templates']
